99aa1733c0e5ff5507a98f0f74cc831191e3b4e2
Chanif Rusydi <chanif.rusydi@gmail.com>
2023-05-03 11:09:39 +0700

    working on image_stitching.py

--- image_stitching.py
+++ image_stitching.py
@@ -73,6 +73,8 @@ import time
 # camera2.release()
 # # Destroy all the windows
 # cv2.destroyAllWindows()
+
+
 # import cv2
 # import threading
 
@@ -111,121 +113,178 @@ import time
 # thread2.start()
 
  
-import torch
-import numpy as np
-import cv2
-from time import time
-from ultralytics import YOLO
+# import torch
+# import numpy as np
+# import cv2
+# from time import time
+# from ultralytics import YOLO
 
-import supervision as sv
+# import supervision as sv
 
 
-class ObjectDetection:
+# class ObjectDetection:
 
-    def __init__(self, capture_index):
+#     def __init__(self, capture_index):
        
-        self.capture_index = capture_index
+#         self.capture_index = capture_index
         
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print("Using Device: ", self.device)
+#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#         print("Using Device: ", self.device)
         
-        self.model = self.load_model()
+#         self.model = self.load_model()
         
-        self.CLASS_NAMES_DICT = self.model.model.names
+#         self.CLASS_NAMES_DICT = self.model.model.names
     
-        self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
+#         self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
     
 
-    def load_model(self):
+#     def load_model(self):
        
-        # model = YOLO("yolov8m.pt")  # load a pretrained YOLOv8n model
-        model = YOLO("yolov8n.pt")
-        model.fuse()
+#         # model = YOLO("yolov8m.pt")  # load a pretrained YOLOv8n model
+#         model = YOLO("yolov8n.pt")
+#         model.fuse()
     
-        return model
+#         return model
 
 
-    def predict(self, frame):
+#     def predict(self, frame):
        
-        results = self.model(frame)
+#         results = self.model(frame)
         
-        return results
+#         return results
     
 
-    def plot_bboxes(self, results, frame):
+#     def plot_bboxes(self, results, frame):
         
-        xyxys = []
-        confidences = []
-        class_ids = []
+#         xyxys = []
+#         confidences = []
+#         class_ids = []
         
-         # Extract detections for person class
-        for result in results:
-            boxes = result.boxes.cpu().numpy()
-            class_id = boxes.cls[0]
-            conf = boxes.conf[0]
-            xyxy = boxes.xyxy[0]
+#          # Extract detections for person class
+#         for result in results:
+#             boxes = result.boxes.cpu().numpy()
+#             class_id = boxes.cls[0]
+#             conf = boxes.conf[0]
+#             xyxy = boxes.xyxy[0]
 
-            if class_id == 0.0:
+#             if class_id == 0.0:
           
-              xyxys.append(result.boxes.xyxy.cpu().numpy())
-              confidences.append(result.boxes.conf.cpu().numpy())
-              class_ids.append(result.boxes.cls.cpu().numpy().astype(int))
+#               xyxys.append(result.boxes.xyxy.cpu().numpy())
+#               confidences.append(result.boxes.conf.cpu().numpy())
+#               class_ids.append(result.boxes.cls.cpu().numpy().astype(int))
             
         
-        # Setup detections for visualization
-        detections = sv.Detections(
-                    xyxy=results[0].boxes.xyxy.cpu().numpy(),
-                    confidence=results[0].boxes.conf.cpu().numpy(),
-                    class_id=results[0].boxes.cls.cpu().numpy().astype(int),
-                    )
+#         # Setup detections for visualization
+#         detections = sv.Detections(
+#                     xyxy=results[0].boxes.xyxy.cpu().numpy(),
+#                     confidence=results[0].boxes.conf.cpu().numpy(),
+#                     class_id=results[0].boxes.cls.cpu().numpy().astype(int),
+#                     )
         
     
-        # Format custom labels
-        self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
-        for _, confidence, class_id, tracker_id
-        in detections]
+#         # Format custom labels
+#         self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
+#         for _, confidence, class_id, tracker_id
+#         in detections]
         
-        # Annotate and display frame
-        frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
+#         # Annotate and display frame
+#         frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
         
-        return frame
+#         return frame
     
     
     
-    def __call__(self):
+#     def __call__(self):
 
-        cap = cv2.VideoCapture(self.capture_index)
-        assert cap.isOpened()
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+#         cap = cv2.VideoCapture(self.capture_index)
+#         assert cap.isOpened()
+#         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+#         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
       
-        while True:
+#         while True:
           
-            start_time = time()
+#             start_time = time()
+            
+#             ret, frame = cap.read()
+#             assert ret
+            
+#             results = self.predict(frame)
+#             frame = self.plot_bboxes(results, frame)
+            
+#             end_time = time()
+#             fps = 1/np.round(end_time - start_time, 2)
+             
+#             cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            
+#             cv2.imshow('YOLOv8 Detection', frame)
+ 
+#             if cv2.waitKey(5) & 0xFF == 27:
+                
+#                 break
+        
+#         cap.release()
+#         cv2.destroyAllWindows()
+        
+        
+    
+# detector = ObjectDetection(capture_index=1)
+# detector()
+
+import device
+import cv2
+
+# def select_camera(last_index, camera_name):
+#     for camera_name_index in camera_name:
+#         if camera_name_index == "Logi C270 WebCam":
+#             camera_index= last_index
+
+
+def open_camera(index):
+    cap = cv2.VideoCapture(index)
+    return cap
+
+def main():
+    # print OpenCV version
+    print("OpenCV version: " + cv2.__version__)
+
+    # Get camera list
+    device_list = device.getDeviceList()
+    index = 0
+
+    for camera in device_list:
+        print(str(index) + ': ' + camera[0])
+        
+        index += 1
+
+    last_index = index - 1
+
+    if last_index < 0:
+        print("No device is connected")
+        return
+
+    # Select a camera
+    # camera_number = select_camera(last_index)
+    
+    # Open camera
+    cap = open_camera(0)
+
+    if cap.isOpened():
+        width = cap.get(3) # Frame Width
+        height = cap.get(4) # Frame Height
+        print('Default width: ' + str(width) + ', height: ' + str(height))
+
+        while True:
             
             ret, frame = cap.read()
-            assert ret
-            
-            results = self.predict(frame)
-            frame = self.plot_bboxes(results, frame)
-            
-            end_time = time()
-            fps = 1/np.round(end_time - start_time, 2)
-             
-            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-            
-            cv2.imshow('YOLOv8 Detection', frame)
- 
-            if cv2.waitKey(5) & 0xFF == 27:
-                
+            cv2.imshow("frame", frame)
+
+            # key: 'ESC'
+            key = cv2.waitKey(20)
+            if key == 27:
                 break
-        
-        cap.release()
-        cv2.destroyAllWindows()
-        
-        
-    
-detector = ObjectDetection(capture_index=1)
-detector()
 
+        cap.release() 
+        cv2.destroyAllWindows() 
 
+if __name__ == "__main__":
+    main()
