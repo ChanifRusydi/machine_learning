9be161269bf06797cce912a7539fc15a3f4e2a83
Chanif Rusydi <chanif.rusydi@gmail.com>
2023-05-25 04:33:53 +0700

    working on upload api

--- api/main.py
+++ api/main.py
@@ -1,15 +1,44 @@
 import os
 
 from flask import Flask
-
+from google.cloud import storage
+import datetime
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+def generate_signed_post_policy_v4(bucket_name, blob_name):
+    """Generates a v4 POST Policy and prints an HTML form."""
+    # bucket_name = 'your-bucket-name'
+    # blob_name = 'your-object-name'
+
+    storage_client = storage.Client()
+
+    policy = storage_client.generate_signed_post_policy_v4(
+        bucket_name,
+        blob_name,
+        expiration=datetime.timedelta(minutes=10),
+        fields={
+          'x-goog-meta-test': 'data'
+        }
+    )
+
+    # Create an HTML form with the provided policy
+    header = "<form action='{}' method='POST' enctype='multipart/form-data'>\n"
+    form = header.format(policy["url"])
+
+    # Include all fields returned in the HTML form as they're required
+    for key, value in policy["fields"].items():
+        form += f"  <input name='{key}' value='{value}' type='hidden'/>\n"
+
+    form += "  <input type='file' name='file'/><br />\n"
+    form += "  <input type='submit' value='Upload File' /><br />\n"
+    form += "</form>"
+
+    print(form)
+
+    return form
+    
 
 
 if __name__ == "__main__":
