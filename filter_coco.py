39bb03e98e1d64abafaa64c9f44bb3f2dce01a8e
Chanif Rusydi <chanif.rusydi@gmail.com>
2023-06-24 05:50:45 +0700

    filtering coco dataset

--- filter_coco.py
+++ filter_coco.py
@@ -3,8 +3,14 @@ import os
 
 
 
-coco_names= [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-         'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+coco_names= [ 'person', 'bicycle',
+ 'car', 'motorcycle', 
+ 'airplane', 'bus', 
+ 'train', 'truck',
+  'boat', 'traffic light',
+
+         'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 
+         'horse', 'sheep', 'cow',
          'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
          'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
          'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
@@ -21,14 +27,14 @@ for index, names in enumerate(coco_names):
         print(index, names)
         index_list.append(index)
 
-# list_index=[0,1,2,3,5,6,7,9,10,15,16]
-list_index=["0","1","2","3","5","6","7","9","10","15","16"]
+# list_index=[0,1,2,3,5,6,7,9,10,11,15,16]
+list_index=["0","1","2","3","5","6","7","9","10","11","15","16"]
 
-# label_base_path = r'C:\Users\User\Documents\machine_learning\yolov7\data\coco\labels\train2017'
-# label_destination_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco_traffic\train\labels'
+label_base_path = r'C:\Users\User\Documents\machine_learning\yolov7\data\coco\labels\train2017'
+label_destination_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco_traffic\train\labels'
 
-# image_base_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco\images\train2017'
-# image_destination_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco_traffic\train\images'
+image_base_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco\images\train2017'
+image_destination_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco_traffic\train\images'
 
 # label_base_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco\labels\val2017'
 # label_destination_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\coco_traffic\val\labels'
@@ -41,12 +47,12 @@ def copy_labels(source,destination):
         copy(source,destination)
     except: pass
     
-# def copy_images(source,destination):
-#     try:
-#         copy(source,destination)
-#     except FileNotFoundError:
-#         return source + " not found"
-#         print("File not found")
+def copy_images(source,destination):
+    try:
+        copy(source,destination)
+    except FileNotFoundError:
+        return source + " not found"
+        print("File not found")
 # filename='000000425481.txt'
 # file_number = filename.split('.')[0]
 # jpg_file=file_number+'.jpg'
@@ -80,12 +86,32 @@ def copy_labels(source,destination):
 #     if True in file_in_list_index:
 #         if os.path.exists(os.path.join(image_base_path,jpg_file)):
 #             copy_labels(os.path.join(label_base_path,file), os.path.join(label_destination_path,file))
-#             # jpg_file=file_number+'.jpg'
-#             # copy_images(os.path.join(image_base_path,jpg_file), os.path.join(image_destination_path,jpg_file))
+#             jpg_file=file_number+'.jpg'
+#             copy_images(os.path.join(image_base_path,jpg_file), os.path.join(image_destination_path,jpg_file))
 #     file_in_list_index.clear()
 #     if file_index==10:
 #         break
-test_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\test'
+
+# test_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\test'
+# for file_index,file in enumerate(os.listdir(label_destination_path)):
+#     print('\n',file)
+#     with open(os.path.join(label_destination_path,file),'r') as f:
+#         file_number = file.split('.')[0]
+#         lines = f.readlines()
+#         print(lines)
+#     with open(os.path.join(label_destination_path,file),'w') as file_write:
+#         for line_index,line in enumerate(lines):
+#             print(line)
+#             single_lines=line.split(' ')
+#             print(single_lines[0])
+#             if single_lines[0] in list_index:
+#                 file_write.write(line)
+
+
+
+
+
+# test_path=r'C:\Users\User\Documents\machine_learning\yolov7\data\test'
 for file_index,file in enumerate(os.listdir(label_destination_path)):
     print('\n',file)
     with open(os.path.join(label_destination_path,file),'r') as f:
@@ -97,9 +123,45 @@ for file_index,file in enumerate(os.listdir(label_destination_path)):
             print(line)
             single_lines=line.split(' ')
             print(single_lines[0])
-            print(single_lines[0],True if single_lines[0] in list_index else False)
-            if single_lines[0] in list_index:
-                file_write.write(line)
-    
-                 
-                    
+            if single_lines[0]=="11":
+                single_lines[0]="9"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="5":
+                single_lines[0]="4"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="6":
+                single_lines[0]="5"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="7":
+                single_lines[0]="6"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="9":
+                single_lines[0]="7"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="10":
+                single_lines[0]="8"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="15":
+                single_lines[0]="10"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            elif single_lines[0]=="16":
+                single_lines[0]="11"
+                new_line=" ".join(single_lines)
+                print(new_line)
+                file_write.write(new_line)
+            else:
+                file_write.write(line)        
