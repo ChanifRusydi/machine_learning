70ab786fea714c73d72d0bb37ab25b2a1eccca1b
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-07-17 23:27:13 +0700

    working on image_stitching

--- image_stitching.py
+++ image_stitching.py
@@ -5,93 +5,66 @@ import time
 import platform
 import sys
 
-parser = argparse.ArgumentParser(description='Image Stitching')
-parser.add_argument('--image', help='image stitching on image files')
-parser.add_argument('--webcam', default=0, type=int, help='image stitching on live video from webcam')
-
-
-if platform.system() == 'Windows':
-    import device
-
-elif platform.system() == 'Linux':
-    print('Linux')
-elif platform.system() == 'Darwin':
-    print('Mac')
-
-
-# def select_camera(last_index, camera_name):
-#     for camera_name_index in camera_name:
-#         if camera_name_index == "Logi C270 WebCam":
-#             camera_index= last_index
-
+def image_stitching(args):
+    cv2.AKAZE_create()
+    cv2.KAZE_create()
+    cv2.ORB_create()
 
 def open_camera(index):
     cap = cv2.VideoCapture(index)
     return cap
 
-def main():
+def main(args):
     # print OpenCV version
     print("OpenCV version: " + cv2.__version__)
 
-    # Get camera list
-    device_list = device.getDeviceList()
-    index = 0
+    if args.mode == 'image':
+        # Read input image
+        if args.image is None:
+            print('Error: --image argument is required when --mode is set to "image"')
+            return
+        img = cv2.imread(args.image)
+        if img is None:
+            print(f'Error: could not read image file "{args.image}"')
+            return
+        # Process input image
+        # ...
+    elif args.mode == 'camera':
+        # Open camera
+        cap = open_camera(0)
+        cap1 = open_camera(1)
 
-    for camera in device_list:
-        print(str(index) + ': ' + camera[0])
+        if cap.isOpened():
+            width = cap.get(3) # Frame Width
+            height = cap.get(4) # Frame Height
+            # Process camera input
+            # ...
+    elif args.mode == 'video':
+        # Open video file
+        if args.video is None:
+            print('Error: --video argument is required when --mode is set to "video"')
+            return 'Error: --video argument is required when --mode is set to "video"'
         
-        index += 1
+        video1 = cv2.VideoCapture(args.video[0])
+        if not cap.isOpened():
+            print(f'Error: could not open video file "{args.video}"')
+            return
+        # Process video file
+        # ...'
+        fps = cap.get(cv2.CAP_PROP_FPS)
 
-    last_index = index - 1
-
-    if last_index < 0:
-        print("No device is connected")
-        return
-
-    # Select a camera
-    # camera_number = select_camera(last_index)
     
-    # Open camera
-    cap = open_camera(0)
-    cap1 = open_camera(1)
-
-    if cap.isOpened():
-        width = cap.get(3) # Frame Width
-        height = cap.get(4) # Frame Height
-        print('Default width: ' + str(width) + ', height: ' + str(height))
-
-        while True:
-            
-            ret, frame = cap.read()
-            ret1, frame1 = cap1.read()
-            if ret == False or ret1 == False:
-                print("No camera")
-                break
-            # else:
-            #     stitching=cv2.Stitcher.create()
-            #     status,frame2=stitching.stitch((frame,frame1))
-            #     if status==0:
-            #         cv2.imshow('frame',frame2)
-            #     else:
-            #         print("Error")
-            #         break
-            # Display the resulting frame
-            # side_by_side = cv2.hconcat([frame, frame1])
-            # cv2.imshow('frame', side_by_side)
-
-            # key: 'ESC'
-            cv2.imwrite('camera1.jpg',frame)
-            key = cv2.waitKey(20)
-            if key == 27:
-                break
-
-        cap.release()
-        cap1.release() 
-        cv2.destroyAllWindows() 
-
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Image Stitching')
+    parser.add_argument('--mode', default='image', type=str, help='camera or video or image')
+    parser.add_argument('--image', type=str,nargs='+',help='path to input image file')
+    parser.add_argument('--camera', type=int, default=[0,1], help='camera index')
+    parser.add_argument('--video', type=str, help='path to input video file')
+    args = parser.parse_args()
+    print(args.mode)
+    print(args.image)
+    print(args.camera)
+    main(args)
 # import cv2
 # image1=cv2.imread('image1.jpg')
 # image2=cv2.imread('image2.jpg')
