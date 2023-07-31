5bd0a3ab0e3e913f2d97ea6c47e4951aa8660c0b
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-07-31 17:55:55 +0700

    working on gui app

--- streamlit_app/pages/Image.py
+++ streamlit_app/pages/Image.py
@@ -1,3 +1,11 @@
 import streamlit as st
-
+import  cv2
 st.title("Image Page")
+with st.container():
+    image1_placeholder, image2_placeholder = st.columns(2)
+    image1_placeholder.header("Image 1")
+    image2_placeholder.header("Image 2")
+    image1 = cv2.imread('../../image1_60_left.jpg')
+    image2 = cv2.imread('../../image1_60_right.jpg')
+image = cv2.hconcat([image1, image2])
+st.image(image, caption='Side by Side Image')
--- test.py
+++ test.py
@@ -149,60 +149,45 @@
 #     video_to_frames(input_loc, output_loc)
 
 # Initiate SIFT detector
-# from __future__ import print_function
-# import cv2 as cv
-# import numpy as np
-# import argparse
+from __future__ import print_function
+import cv2 as cv
+import numpy as np
+import argparse
 
-# parser = argparse.ArgumentParser(description='Code for Feature Matching with FLANN tutorial.')
-# parser.add_argument('--input1', help='Path to input image 1.', default='image1_60_left.jpg')
-# parser.add_argument('--input2', help='Path to input image 2.', default='image1_60_right.jpg')
-# args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Code for Feature Matching with FLANN tutorial.')
+parser.add_argument('--input1', help='Path to input image 1.', default='image1_60_left.jpg')
+parser.add_argument('--input2', help='Path to input image 2.', default='image1_60_right.jpg')
+args = parser.parse_args()
 
-# img1 = cv.imread(cv.samples.findFile(args.input1), cv.IMREAD_GRAYSCALE)
-# img2 = cv.imread(cv.samples.findFile(args.input2), cv.IMREAD_GRAYSCALE)
-# if img1 is None or img2 is None:
-#     print('Could not open or find the images!')
-#     exit(0)
+img1 = cv.imread(cv.samples.findFile(args.input1), cv.IMREAD_GRAYSCALE)
+img2 = cv.imread(cv.samples.findFile(args.input2), cv.IMREAD_GRAYSCALE)
+if img1 is None or img2 is None:
+    print('Could not open or find the images!')
+    exit(0)
 
-# #-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-# minHessian = 400
-# detector = cv.ORB_create()
-# keypoints1, descriptors1 = detector.detectAndCompute(img1, None)
-# keypoints2, descriptors2 = detector.detectAndCompute(img2, None)
+#-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+minHessian = 400
+detector = cv.ORB_create()
+keypoints1, descriptors1 = detector.detectAndCompute(img1, None)
+keypoints2, descriptors2 = detector.detectAndCompute(img2, None)
 
-# #-- Step 2: Matching descriptor vectors with a FLANN based matcher
-# # Since SURF is a floating-point descriptor NORM_L2 is used
-# matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
-# knn_matches = matcher.knnMatch(descriptors1, descriptors2, 2)
+#-- Step 2: Matching descriptor vectors with a FLANN based matcher
+# Since SURF is a floating-point descriptor NORM_L2 is used
+matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
+knn_matches = matcher.knnMatch(descriptors1, descriptors2, 2)
 
-# #-- Filter matches using the Lowe's ratio test
-# ratio_thresh = 0.7
-# good_matches = []
-# for m,n in knn_matches:
-#     if m.distance < ratio_thresh * n.distance:
-#         good_matches.append(m)
+#-- Filter matches using the Lowe's ratio test
+ratio_thresh = 0.7
+good_matches = []
+for m,n in knn_matches:
+    if m.distance < ratio_thresh * n.distance:
+        good_matches.append(m)
 
-# #-- Draw matches
-# img_matches = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1], 3), dtype=np.uint8)
-# cv.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+#-- Draw matches
+img_matches = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1], 3), dtype=np.uint8)
+cv.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-# #-- Show detected matches
-# cv.imshow('Good Matches', img_matches)
+#-- Show detected matches
+cv.imshow('Good Matches', img_matches)
 
-# cv.waitKey()
-import cv2
-
-cam = cv2.VideoCapture('Video1.mp4')
-cam.getBackendName()
-# while True:
-#     check, frame = cam.read()
-
-#     cv2.imshow('video', frame)
-
-#     key = cv2.waitKey(1)
-#     if key == 27:
-#         break
-
-cam.release()
-cv2.destroyAllWindows()
+cv.waitKey()
