fb3838643e6e6024c9128f0dfd8b316af3901498
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-08-03 13:41:14 +0700

    change script file

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+
 # straight running python script
 # python -m http.server
 # npm run serve
@@ -6,5 +7,5 @@
 # sudo loginctl terminate-seat seat0
 # xinit
 firefox --kiosk http://localhost:8501 
-streamlit run streamlit_app/streamlit_app.py --server.headless true
+streamlit run /home/jetson/streamlit_app/streamlit_app.py --server.headless true
  
