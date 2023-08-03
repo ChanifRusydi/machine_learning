943ea71276a1bca571eabf684a4feb50424a189e
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-08-03 16:23:21 +0700

    working on webcam streamlit gui app

--- streamlit_app/pages/Webcam.py
+++ streamlit_app/pages/Webcam.py
@@ -2,45 +2,65 @@ import streamlit as st
 import cv2
 import numpy as np
 from streamlit_extras.switch_page_button import switch_page
+from yolov8_detect import detect
+
+camera1 = None
+camera2 = None
 
 st.title("Webcam Page")
 back_button  = st.button("Back to Home Page")
 if back_button:
     switch_page("streamlit_app")
+camera1 = cv2.VideoCapture(0)
+camera2 = cv2.VideoCapture(1)
+    
+camera1_placeholder, camera2_placeholder = st.empty()
+stop_button_pressed = st.button("Stop")
+
+ret1, frame1 = camera1.read()
+ret2, frame2 = camera2.read()
+while ret1 and ret2 and not stop_button_pressed:
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+    camera1_placeholder.image(frame1,channels="RGB")
+    camera2_placeholder.image(frame2,channels="RGB")
+
+    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+        camera1.release()
+        camera2.release()
+        break
+while ret1 and not stop_button_pressed:
+    ret1, frame1 = camera1.read()
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+    camera1_placeholder.image(frame1, channels="BGR")
+    if cv2.waitKey(1) & 0xFF == ord('q') or stop_button_pressed:
+        camera1.release()
+        break
+while ret2 and not stop_button_pressed:
+    ret2, frame2 = camera2.read()
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+    camera2_placeholder.image(frame2, channels="BGR")
+    if cv2.waitKey(1) & 0xFF == ord('q') or stop_button_pressed:
+        break
+    
 with st.container():
     camera1_placeholder, camera2_placeholder = st.columns(2)
     camera1_placeholder.header("Camera 1")
     camera2_placeholder.header("Camera 2")
 
-    camera1 = cv2.VideoCapture(0)
-    camera2 = cv2.VideoCapture(1)
-    
-    camera1_placeholder, camera2_placeholder = st.empty()
-    stop_button_pressed = st.button("Stop")
-    
-    ret1, frame1 = camera1.read()
-    ret2, frame2 = camera2.read()
-    while ret1 and ret2 and not stop_button_pressed:
-        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
-        frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(frame,channels="RGB")
 
-        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
-            camera1.release()
-            break
-    while ret1 and not stop_button_pressed:
-        ret1, frame1 = camera1.read()
-        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
-        camera1_placeholder.image(frame1, channels="BGR")
-        if cv2.waitKey(1) & 0xFF == ord('q') or stop_button_pressed:
-            camera1.release()
-            break
-    while ret2 and not stop_button_pressed:
-        ret2, frame2 = camera2.read()
-        frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
-        camera2_placeholder.image(frame2, channels="BGR")
-        if cv2.waitKey(1) & 0xFF == ord('q') or stop_button_pressed:
-            break
+
+    
+with st.container():
+    camera_side_by_side_placeholder = st.empty()
+    if camera1 is None:
+        print("camera1 is None")
+    if camera2 is None:
+        print("camera2 is None")
+
+    image = cv2.hconcat([frame1, frame2])
+    status, image_detect = detect(image)
+    camera_side_by_side_placeholder.image(image, channels="BGR")
 
 camera1.release()
 cv2.destroyAllWindows()
