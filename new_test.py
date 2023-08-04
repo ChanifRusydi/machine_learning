0ad0cfeabd6311efc31a2537b462c751369e5170
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-08-04 11:36:38 +0700

    fixing testing

--- new_test.py
+++ new_test.py
@@ -2,19 +2,26 @@ import cv2
 
 camera1= cv2.VideoCapture(0)
 camera2 = cv2.VideoCapture(1)
-camera1.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+camera1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera1.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+camera2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+camera1.set(cv2.CAP_PROP_FPS, 30)
+camera2.set(cv2.CAP_PROP_FPS, 30)
 # camera1_placeholder, camera2_placeholder = st.columns([0.5, 0.5])
 counter = 0
 while(True):
     status1 = camera1.grab()
+    
     status2 = camera2.grab()
-    # print(status1,status2)
+    print(status1,status2)
     # counter+=1
     
     _,frame1 = camera1.retrieve()
-    cv2.imshow('frame1', frame1)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    _,frame2 = camera2.retrieve()
+    side_by_side = cv2.hconcat([frame1, frame2])
+    cv2.imshow('side by side', side_by_side)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     # break
     # if not status1:
