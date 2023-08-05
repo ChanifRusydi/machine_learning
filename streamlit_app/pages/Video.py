2c231fd648a14adc6b5ed946ab91d0e02435655c
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-08-05 20:55:48 +0700

    video page working

--- streamlit_app/pages/Video.py
+++ streamlit_app/pages/Video.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 from tempfile import NamedTemporaryFile
 import logging
+from yolov8_detect import detect
 
 logging.basicConfig(filename='logfile.txt',filemode='a',format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S',
@@ -10,6 +11,9 @@ logging.info('Enter Video Page')
 
 st.title("Video Page")
 
+video_status1 = False
+video_status2 = False
+
 with st.container():
     video1_placeholder, video2_placeholder = st.columns(2)
     video_upload1 = video1_placeholder.file_uploader("Upload Video 1", type=['mp4'])
@@ -17,25 +21,26 @@ with st.container():
     video1_placeholder.header("Video 1")
     video2_placeholder.header("Video 2")
     if video_upload1 is not None :
-        with NamedTemporaryFile(suffix='mp4') as tmp1:
-            tmp1.write(video_upload1.read())
-            open_video1 = cv2.VideoCapture(tmp1.name)
+            with open('video1.mp4', 'wb') as f:
+                f.write(video_upload1.getbuffer())
+             
+            video_status1 = True
     else:
         logging.info('Uploaded Video Not Found')
-        open_video1 = cv2.VideoCapture('../../image1_60_left.mp4')
+        # open_video1 = cv2.VideoCapture('../../image1_60_left.mp4')
     
     if video_upload2 is not None:
-        with NamedTemporaryFile(suffix='mp4') as tmp2:
-            tmp2.write(video_upload2.read())
-            open_video2 = cv2.VideoCapture(tmp2.name)
-    
+        with open('video2.mp4', 'wb') as f:
+            f.write(video_upload2.getbuffer())
+        video_status2 = True   
     video1_placeholder.video(video_upload1)
     video2_placeholder.video(video_upload2)
 
     # open_video1 = cv2.VideoCapture('../../image1_60_left.mp4')   
     # open_video2 = cv2.VideoCapture('../../image1_60_right.mp4')
-    ret1, frame1 = open_video1.read()
-    ret2, frame2 = open_video2.read()
+    
+    # ret1, frame1 = open_video1.read()
+    # ret2, frame2 = open_video2.read()
             
             
     #         video1_placeholder.image(frame1, channels="BGR")
@@ -63,3 +68,28 @@ with st.container():
     # open_video1.release()
     # open_video2.release()
 
+with st.container():
+    side_by_side_placeholder = st.empty()
+    side_by_side_placeholder.header("Side by Side Video")
+    if video_status1 and video_status2:
+        open_video1 = cv2.VideoCapture('video1.mp4')
+        open_video2 = cv2.VideoCapture('video2.mp4')
+        while True:
+            ret1, frame1 = open_video1.read()
+            ret2, frame2 = open_video2.read()
+            if not ret1 or not ret2:
+                break
+            frame1 = cv2.cvtColor(frame1, cv2.COLOR_RGB2BGR)
+            frame2 = cv2.cvtColor(frame2, cv2.COLOR_RGB2BGR)
+            if frame1.shape != frame2.shape:
+                side_by_side_placeholder.subheader("Please open video with same shape")
+            else:
+                image = cv2.hconcat([frame1, frame2])
+                status,image = detect(image)
+                side_by_side_placeholder.image(image, channels="BGR")
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        open_video1.release()
+        open_video2.release()
+    else:
+        side_by_side_placeholder.subheader("Please upload both video")
