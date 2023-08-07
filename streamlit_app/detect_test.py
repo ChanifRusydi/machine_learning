09473edfbf48e4b37d60b32c30d0480f20b5830c
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2023-08-07 15:17:31 +0700

    add image detection

--- streamlit_app/detect_test.py
+++ streamlit_app/detect_test.py
@@ -1,6 +1,9 @@
 import cv2
+import os
 from yolov8_detect import detect
-
-image1 = cv2.imread('bus.jpg ')
-status, image_detect = detect(image1)
-cv2.imwrite('image_detect_result.jpg', image_detect)
+image_list= ['pagi.png', 'siang.png','sore.png','malam.png']
+for image in image_list:
+    image1 = cv2.imread(image)
+    status, image_detect = detect(image1)
+    file_name = os.path.basename(image) + '_detect.png'
+    cv2.imwrite(filename=file_name,img=image_detect)
